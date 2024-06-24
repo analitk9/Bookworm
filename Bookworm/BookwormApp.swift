74cb725df409fb58a782a231b6d81a8ddb1e3b64
Evdokimov Denis <analitik9@gmail.com>
2024-06-24 13:58:40 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Denis Evdokimov on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Denis Evdokimov on 6/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
